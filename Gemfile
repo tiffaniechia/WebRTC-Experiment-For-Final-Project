48360b2ed1816b04ffd41864d2090f233f8f11fe
tiffaniechia <tiffaniekyrachia@gmail.com>
2014-04-25 15:59:56 +0100

    heroku fixes

--- Gemfile
+++ Gemfile
@@ -1,6 +1,10 @@
 source 'https://rubygems.org'
 
 
+group :production do 
+  gem 'rails_12factor'
+end  
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.0'
 # Use sqlite3 as the database for Active Record
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
-
+  root 'chats#index'
   resources :chats
-  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
