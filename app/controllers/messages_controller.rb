e6e050ef4517dca7168040a223339cad08e575c2
tiffaniechia <tiffaniekyrachia@gmail.com>
2014-05-01 02:17:03 +0100

    added basic analog clock functionality for experimentation

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,7 @@ class MessagesController < ApplicationController
 
   def create
     # username: current_user.email, 
-    WebsocketRails[:chat].trigger 'new', { message: params[:message]}
+    WebsocketRails[:chat].trigger 'new', { message: params[:message], user_name: 'frank'}
     render nothing: true
   end
 
